36cd0a425fc03acd498dde97a4d33b4f0cd5a899
saima.khan105 <saima.khan105@outlook.com>
2022-07-28 19:51:07 -0500

    first commit

---
+++ Extensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Numerics;
+
+public static class Extensions
+{
+    public static string ToWords(this BigInteger bi)
+    {
+        try
+        {
+            return Convert((double)bi);
+        }
+        catch (Exception ex)
+        {
+            // TODO: handle exception  
+        }
+        return "";
+    }
+    private static String Convert(double n) 
+    {
+        string[] numbersArr = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        string[] tensArr = new string[] { "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninty" };
+        string[] suffixesArr = new string[] { "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion", "Undecillion", "Duodecillion", "Tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septdecillion", "Octodecillion", "Novemdecillion", "Vigintillion" };
+        string words = "";
+
+        bool tens = false;
+
+        if (n < 0)
+        {
+            words += "negative ";
+            n *= -1;
+        }
+
+        int power = (suffixesArr.Length + 1) * 3;
+
+        while (power > 3)
+        {
+            double pow = Math.Pow(10, power);
+            if (n >= pow)
+            {
+                if (n % pow > 0)
+                {
+                    words += Convert(Math.Floor(n / pow)) + " " + suffixesArr[(power / 3) - 1] + ", ";
+                }
+                else if (n % pow == 0)
+                {
+                    words += Convert(Math.Floor(n / pow)) + " " + suffixesArr[(power / 3) - 1];
+                }
+                n %= pow;
+            }
+            power -= 3;
+        }
+        if (n >= 1000)
+        {
+            if (n % 1000 > 0) words += Convert(Math.Floor(n / 1000)) + " thousand, ";
+            else words += Convert(Math.Floor(n / 1000)) + " thousand";
+            n %= 1000;
+        }
+        if (0 <= n && n <= 999)
+        {
+            if ((int)n / 100 > 0)
+            {
+                words += Convert(Math.Floor(n / 100)) + " hundred";
+                n %= 100;
+            }
+            if ((int)n / 10 > 1)
+            {
+                if (words != "")
+                    words += " ";
+                words += tensArr[(int)n / 10 - 2];
+                tens = true;
+                n %= 10;
+            }
+
+            if (n < 20 && n > 0)
+            {
+                if (words != "" && tens == false)
+                    words += " ";
+                words += (tens ? "-" + numbersArr[(int)n - 1] : numbersArr[(int)n - 1]);
+                n -= Math.Floor(n);
+            }
+        }
+
+        return words;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger i = 18000000;
+            Console.WriteLine(i.ToWords());
+
+            
+            i = 12233442;
+            Console.WriteLine(i.ToWords());
+            
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
